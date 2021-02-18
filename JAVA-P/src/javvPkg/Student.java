2d9103ca311918535ab718b3ff2b4c14371003b4
Test <ree@gmail.com>
2021-02-18 15:27:11 +0530

    Commit

---
+++ JAVA-P/src/javvPkg/Student.java
@@ -0,0 +1,15 @@
+package javvPkg;
+
+public class Student {
+
+		String studentName="ravinder";
+		String studentClass ="tenth";
+		
+		
+		public static void main ()
+		{
+			Student s = new Student();
+			System.out.println(s.studentName);
+		}
+	}
+
